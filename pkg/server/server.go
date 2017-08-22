0f6d9875c4ad31517d4766d4aabc391e4635cd28
Sam Mulube <sam@thingful.net>
2017-08-22 16:18:11 +0100

    Update server package to use go-kit logger

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,33 +2,43 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"net/http"
 	"os"
 
+	"github.com/go-kit/kit/log"
 	"github.com/soheilhy/cmux"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	pb "github.com/thingful/greeter/pkg/greeter"
+	"github.com/thingful/greeter/pkg/version"
 )
 
 const (
 	port = ":5555"
 )
 
-type server struct{}
+type server struct {
+	logger log.Logger
+}
 
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	s.logger.Log("handler", "SayHello", "name", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
 func main() {
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
+	logger = log.With(logger, "version", version.Version, "ts", log.DefaultTimestampUTC)
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		logger.Log("error", err, "port", port, "msg", "failed to listen")
+		os.Exit(1)
 	}
+
+	logger.Log("msg", "starting server")
 
 	// set up our request multiplexer
 	m := cmux.New(lis)
@@ -37,11 +47,12 @@ func main() {
 
 	// create grpc server
 	grpcServer := grpc.NewServer()
-	pb.RegisterGreeterServer(grpcServer, &server{})
+	pb.RegisterGreeterServer(grpcServer, &server{logger: log.With(logger, "protocol", "grpc")})
 
 	// create http mux and server
 	h := http.NewServeMux()
 	h.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		logger.Log("protocol", "http", "path", "/status")
 		fmt.Fprintf(w, "ok")
 	})
 
@@ -55,19 +66,19 @@ func main() {
 	go func() { eps <- grpcServer.Serve(grpcLis) }()
 	go func() { eps <- httpServer.Serve(httpLis) }()
 
-	log.Println("starting multiplexed server on: %s", port)
+	logger.Log("port", port, "starting multiplexed server")
 	err = m.Serve()
 
 	var failed bool
 	if err != nil {
-		log.Println("cmux serve error: %v", err)
+		logger.Log("error", err, "msg", "cmux serve error")
 		failed = true
 	}
 
 	var i int
 	for err := range eps {
 		if err != nil {
-			log.Printf("protocol serve error: %v", err)
+			logger.Log("error", err, "msg", "protocol serve error")
 			failed = true
 		}
 		i++
